9790cccde81b7be1734630fc8a37542e34352691
eduardafillmann <111399806+eduardafillmann@users.noreply.github.com>
2023-06-14 14:42:42 -0300

    .

---
+++ exercícios/ex008/ex008.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+if (vel > 60) { //Condição simples - sem else
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
