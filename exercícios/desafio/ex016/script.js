17a8e701f42ac979b4f07d588cf8dbe5aa03f5e1
eduardafillmann <111399806+eduardafillmann@users.noreply.github.com>
2023-06-16 14:56:11 -0300

    update

---
+++ exercícios/desafio/ex016/script.js
@@ -0,0 +1,1 @@
+alert('Fala tu mano!')
---
+++ exercícios/ex013/ambiente2.js
@@ -0,0 +1,14 @@
+/*
+var c = 1
+do{ 
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+}
+*/
+//---------------------------
+
+for(var c = 1;c <= 5;c++) {
+    console.log(c)
+}
+0
