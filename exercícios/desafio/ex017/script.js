deae4c70426b244aef6840874b13ffb2548b0abf
eduardafillmann <111399806+eduardafillmann@users.noreply.github.com>
2023-06-20 11:57:23 -0300

    .

--- exercícios/desafio/ex017/script.js
+++ exercícios/desafio/ex017/script.js
@@ -1,1 +1,16 @@
-alert('Fala tu mano!')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.ariaValueMax.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${num} x $(c) = ${num*c}`
+            tab.appendChild(item)
+            c++
+        }
+    }    
+}
