5deda8cf297b225b048d0c56d8137b39c4b18056
eduardafillmann <111399806+eduardafillmann@users.noreply.github.com>
2023-06-20 12:18:39 -0300

    ;

--- exercícios/desafio/ex017/script.js
+++ exercícios/desafio/ex017/script.js
@@ -1,14 +1,16 @@
 function tabuada() {
     let num = document.getElementById('txtn')
     let tab = document.getElementById('seltab')
-    if (num.ariaValueMax.length == 0) {
+    if (num.value.length == 0) {
         window.alert('Por favor, digite um número!')
     } else {
         let n = Number(num.value)
         let c = 1
-        while (c <= 10) {
+        tab.innerHTML = ''
+        while (c <= 20) {
             let item = document.createElement('option')
-            item.text = `${num} x $(c) = ${num*c}`
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
             tab.appendChild(item)
             c++
         }
